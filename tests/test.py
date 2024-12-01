0ec1c5939274960bec2c226d2862a226e70a6f1b
Atilia T <atiliat@MacBookAir.attlocal.net>
2024-12-01 07:56:13 -0600

    class documentation

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,15 @@ import unittest
 from plot import plot_normal, plot_line, plot_live 
 
 class TestPlotFunctions(unittest.TestCase): 
+    """ 
+    This class will be used for plotting functions in the 'plot' module 
+
+    This test class tests the functionality of the functions: 
+    - `plot_normal`: Plots 200 points sampled from a standard normal distribution 
+    - `plot_line`: Plots a line given its slope, y-intercept, and x-boundaries 
+    - `plot_live`: Generates live points and updates a graph.
+    
+    """
     def test_normal(self): 
         try: 
             plot_normal() 
